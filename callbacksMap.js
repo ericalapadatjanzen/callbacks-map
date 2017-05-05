20cae5748b2a1907fd7d217b7009657b15667875
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-05 00:23:00 +0000

    used map to find array + 1

---
+++ callbacksMap.js
@@ -0,0 +1,36 @@
+// var numbers = [1, 4, 9];
+// var doubles = numbers.map(function(num) {
+//   return num * 2;
+// });
+// console.log(doubles)
+
+
+var num = [1, 8, 77, 99, 2];
+
+var newArray = num.map(function(num) {
+  return num + 1;
+
+});
+console.log(newArray)
+
+
+
+// MADE THE EXAMPLE WORK!
+// var words = ["ground", "control", "to", "major", "tom"];
+
+// var newArray = words.map(function(word) {
+//   return word.length;
+
+// });
+// console.log(newArray)
+
+
+
+
+
+// Orginal code given - comes out with map undefined
+// var words = ["ground", "control", "to", "major", "tom"];
+
+// map(words, function(word) {
+//   return word.length;
+// });
